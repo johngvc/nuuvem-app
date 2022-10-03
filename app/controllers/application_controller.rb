ded85a579c64e2f04a466d018c2c9226935bf52e
Gabriel Campos <gabriel.campos@grana.capital>
2022-10-02 21:28:39 -0300

    Fixed devise and updated docker compose to allow outside connections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true, with: :exception, unless: -> { request.format.json? }
+protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
+
+
 end
