8c111bb0087d15cdf6ec998f1533a06083a1f2a3
Gabriel Campos <gabriel.campos@grana.capital>
2022-10-04 01:36:39 -0300

    Small fixes

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,6 +1,10 @@
 ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
+  action_item :view_site do
+    link_to "Submit new sale report", new_admin_sales_report_path
+  end
+
   content title: proc { I18n.t('active_admin.dashboard') } do
     div class: 'blank_slate_container', id: 'dashboard_default_message' do
       span class: 'blank_slate' do
@@ -8,15 +12,16 @@ ActiveAdmin.register_page 'Dashboard' do
         small I18n.t('active_admin.dashboard_welcome.call_to_action')
       end
     end
-
+    
     columns do
       column do
         panel I18n.t('active_admin.sales') do
           para("#{I18n.t('active_admin.total_gross_income')}: #{Purchase.total_gross_income}")
         end
 
+        # TODO Remove query from here, fix order of reports shown
         panel I18n.t('active_admin.sales_reports_history') do
-          table_for SalesReports.last(10) do
+          table_for SalesReports.all.order(updated_at: :asc).last(10) do
             column :file_name
             column :gross_sum
             column :updated_at
