da3f1383305dc4e5f66ee203e396dda2032f3d6b
Gabriel Campos <gabriel.campos@grana.capital>
2022-10-04 01:45:59 -0300

    Added items active admin view

---
+++ app/admin/items.rb
@@ -0,0 +1,45 @@
+ActiveAdmin.register Item do
+  permit_params :name, :price
+
+  actions :all
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :price
+    column :merchant
+
+    actions
+  end
+
+  filter :name
+  filter :price
+  filter :merchant
+  filter :created_at
+
+  show do
+    tabs do
+      tab I18n.t('active_admin.overview') do
+        panel I18n.t('active_admin.general') do
+          attributes_table_for item do
+            row :id
+            row :name
+            row :price
+            row :merchant
+          end
+        end
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs do
+        f.input :name
+        f.input :price
+        f.input :merchant
+    end
+
+    actions
+  end
+end
